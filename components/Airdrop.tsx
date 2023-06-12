f90f911e3f90e954c4c696e2cec3f538ba026720
0xmaximize <135501343+0xmaximize@users.noreply.github.com>
2023-06-12 21:48:30 +0700

    update

--- components/Airdrop.tsx
+++ components/Airdrop.tsx
@@ -19,9 +19,16 @@ const Airdrop: NextPage = () => {
             A total of 180,000,000,000,000,000 ARBPUPPY tokens are available to be claimed by those who have the <a href="https://www.arbpuppy.city/NFT" target="_blank">AIPUPPY NFT</a>, ARB Claimer & <a href="https://forms.gle/zS8Z9kWpRHb6fw4p9" target="_blank">Genesis Tier</a> 
             <br />
             <br />
+        
             ARBPUPPY tokens that have not been claimed within 31 days will be used for the Community Long-Term Incentive Reward Program. The ARBPUPPY will be distributed to the top contributors of Arbitrum community and burned. 
-          
+
             </p>
+        <div className={styles.title}>
+  
+        <a href="#">
+         Airdrop claims will be live until: 2023.06.14 (UTC+0)
+         </a>
+        </div>
             <TAB />
          
           
